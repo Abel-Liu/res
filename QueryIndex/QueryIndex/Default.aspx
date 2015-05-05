80554563b89e8c7e15296bee12ae00c840fe3db1
Abel-Liu <abel.liu.china@outlook.com>
2015-05-05 22:17:23 +0800

    query index

---
+++ QueryIndex/QueryIndex/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="QueryIndex._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Storage:<asp:TextBox runat="server" ID="txtStorage"></asp:TextBox>
+            <br />
+            Contents:<asp:TextBox runat="server" ID="txtContent"></asp:TextBox>
+            <asp:Button runat="server" ID="btnSearch" Text="Go" OnClick="btnSearch_Click" />
+            <br />
+            <br />
+            <asp:DataGrid runat="server" ID="grid"></asp:DataGrid>
+        </div>
+    </form>
+</body>
+</html>
---
+++ QueryIndex/QueryIndex/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace QueryIndex
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            //var strQuery = "Select DocTitle,Filename,Size,PATH,URL from Scope() where FREETEXT(Contents,'" + this.txtContent.Text + "')";
+            var strQuery = "Select DocTitle,Filename,Size,PATH,URL from Scope() where Filename='" + this.txtContent.Text + "'";
+            string connstring = string.Format("Provider=\"MSIDXS\";Data Source=\"{0}\"", this.txtStorage.Text);
+
+            System.Data.OleDb.OleDbConnection conn = new System.Data.OleDb.OleDbConnection(connstring);
+            conn.Open();
+
+            System.Data.OleDb.OleDbDataAdapter cmd = new System.Data.OleDb.OleDbDataAdapter(strQuery, conn);
+
+            DataTable dt = new DataTable();
+
+            cmd.Fill(dt);
+            conn.Close();
+
+            this.txtContent.Text = dt.Rows.Count.ToString();
+            DataView source = new DataView(dt);
+            grid.DataSource = source;
+            grid.DataBind();
+
+
+        }
+    }
+}
