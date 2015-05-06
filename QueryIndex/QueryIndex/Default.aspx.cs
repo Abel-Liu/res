0af7c1498a7c1fc3af789e48a15b0297ddc99ed2
Abel <abel.liu.china@outlook.com>
2015-05-06 10:07:44 +0800

    temp

--- QueryIndex/QueryIndex/Default.aspx.cs
+++ QueryIndex/QueryIndex/Default.aspx.cs
@@ -16,8 +16,7 @@ namespace QueryIndex
 
         protected void btnSearch_Click(object sender, EventArgs e)
         {
-            //var strQuery = "Select DocTitle,Filename,Size,PATH,URL from Scope() where FREETEXT(Contents,'" + this.txtContent.Text + "')";
-            var strQuery = "Select DocTitle,Filename,Size,PATH,URL from Scope() where Filename='" + this.txtContent.Text + "'";
+            
             string connstring = string.Format("Provider=\"MSIDXS\";Data Source=\"{0}\"", this.txtStorage.Text);
 
             System.Data.OleDb.OleDbConnection conn = new System.Data.OleDb.OleDbConnection(connstring);
